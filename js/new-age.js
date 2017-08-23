950d1dbb10f6e04b8cfac6df0144558dc3895d21
Leo Bensman <work@Leos-MBP.router>
2017-08-23 14:10:56 -0400

    reorganization

--- js/new-age.js
+++ js/new-age.js
@@ -28,4 +28,24 @@
         }
     })
 
+    // Closes the sidebar menu
+    $("#menu-close").click(function(e) {
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
+    });
+    // Opens the sidebar menu
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+
+
+
 })(jQuery); // End of use strict
+
